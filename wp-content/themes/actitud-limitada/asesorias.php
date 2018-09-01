de6ae287bcaf4450e7f2290e22e2dca3f4f709f2
Francisca Carrasco <francisca.carrasco.farias@gmail.com>
2018-09-01 18:54:59 -0300

    Agregada pagina de talleres.php

---
+++ wp-content/themes/actitud-limitada/asesorias.php
@@ -0,0 +1,58 @@
+<?php get_header() ?>
+    <!-- Header titulo-->
+    <div class="container-block contenedor_morado p-5 text-center mx-auto">
+        <div class="col-lg-12">
+            <h3 class="py-1 pb-2">Asesorías</h3>
+        </div>
+        <div class="col-lg-6 mx-auto">
+          <p>Servicios profesionales de reclutamiento, que se adecuan a las necesidades de tu empresa.</p>
+        </div> 
+    </div>
+
+    <!-- Servicios Reclutamiento -->
+    <div class="container py-5">
+      <div class="col-lg-12">
+          <div class="row">
+            <!-- Titulo -->
+            <div class="col-lg-12 mx-auto mb-2 ">
+              <h3 class="pb-2 h3linea">Tipos de Asesorías</h3>
+            </div>
+            <!-- Reclutamiento -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Mejora tu currículum</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Entrevista presencial</h5>
+              <p>Tú nos entregas los candidatos que consideres deben ser parte de tu empresa, nosotros los evaluamos, entrevistamos y entregamos una terna para que escojas a quien más te haga sentido. (También te haremos sugerencias).</p>
+            </div>
+            <!-- Reclutamiento y selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Entrevista por competencias</h5>
+              <p>Realizamos el proceso completo. Levantamiento de Perfil, Reclutamiento y Selección. </p>
+            </div>
+            <!-- Reclutamiento y selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Entrevista Psicologica</h5>
+              <p>Realizamos el proceso completo. Levantamiento de Perfil, Reclutamiento y Selección. </p>
+            </div>
+            <!-- Reclutamiento y selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Entrevista Personalizada</h5>
+              <p>Realizamos el proceso completo. Levantamiento de Perfil, Reclutamiento y Selección. </p>
+            </div>
+          </div>
+      </div> 
+    </div>
+
+    <div class="container-block container-blue py-5 text-center">
+        <div class="col-lg-10 mx-auto mb-2 ">
+            <h2 class="pb-2">Si necesitas más información, contáctanos</h2>
+        </div>
+        <div class="col-lg-6 mx-auto">
+          <p>Si tienes alguna duda, o deseas cotizar algún servicio puedes contactarnos. En <b>Actitud Ilimitada</b> todo es posible.</p>
+              <a href=""><button class="btn btn-primary btn-md">¡Contáctanos!</button></a>
+        </div>
+    </div>
+<?php get_footer()?>
---
+++ wp-content/themes/actitud-limitada/talleres.php
@@ -0,0 +1,144 @@
+<?php get_header() ?>
+    <!-- Header titulo-->
+    <div class="container-block contenedor_morado p-5 text-center mx-auto">
+        <div class="col-lg-12">
+            <h3 class="py-1 pb-2">Talleres, cursos y capacitaciones</h3>
+        </div>
+        <div class="col-lg-6 mx-auto">
+          <p>Servicios profesionales de reclutamiento, que se adecuan a las necesidades de tu empresa.</p>
+        </div> 
+    </div>
+
+    <!-- Servicios Taller -->
+    <div class="container py-5">
+      <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-12 mx-auto mb-2 ">
+              <h3 class="pb-2 h3linea">Talleres</h3>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Expresión Verbal</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Taller de Voz</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Autoconocimiento</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Autoestima</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Competencias Laborales</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Liderazgo</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Trabajo en equipo</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Comunicación</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Taller de género</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-3 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">3 Competencias</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+          </div>
+      </div> 
+    </div>
+
+    <!-- Servicios Talleres personalizados -->
+    <div class="container ">
+      <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-12 mx-auto mb-2 ">
+              <h3 class="pb-2 h3linea">Relatorías</h3>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Mejora tu actitud</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Mejora tu Presencia</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Mejora tu expresión Vocal y Verbal</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+          </div>
+      </div> 
+    </div>
+
+    <!-- Servicios Capacitaciones -->
+    <div class="container pb-5">
+      <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-12 mx-auto mb-2 ">
+              <h3 class="pb-2 h3linea">Capacitaciones</h3>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Mejorando el trabajo en equipo</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Potenciando mi liderazgo</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">¿Cómo comunico lo que quiero comunicar? </h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+             <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Preparándome para los cambios</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+            <!-- Taller -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul ">Desarrollando mis competencias interpersonales</h5>
+              <p>Ofrecemos servicios profesionales de reclutamiento. Te entregamos el mejor listado de candidatos y tú los entrevistas y decides.</p>
+            </div>
+          </div>
+      </div> 
+    </div>
+
+    <div class="container-block container-blue py-5 text-center">
+        <div class="col-lg-10 mx-auto mb-2 ">
+            <h2 class="pb-2">Si necesitas más información, contáctanos</h2>
+        </div>
+        <div class="col-lg-6 mx-auto">
+          <p>Si tienes alguna duda, o deseas cotizar algún servicio puedes contactarnos. En <b>Actitud Ilimitada</b> todo es posible.</p>
+              <a href=""><button class="btn btn-primary btn-md">¡Contáctanos!</button></a>
+        </div>
+    </div>
+<?php get_footer()?>

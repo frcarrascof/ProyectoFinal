6848942f542677de0235c4f9bfdf7f129f8d2752
Francisca Carrasco <francisca.carrasco.farias@gmail.com>
2018-09-01 19:03:28 -0300

    Agregada pagina eventos.php

---
+++ wp-content/themes/actitud-limitada/eventos.php
@@ -0,0 +1,74 @@
+<?php get_header() ?>
+
+    <!-- Header titulo-->
+    <div class="container-block contenedor_morado p-5 text-center mx-auto">
+        <div class="col-lg-12">
+            <h3 class="py-1 pb-2">Eventos personalizados</h3>
+        </div>
+        <div class="col-lg-6 mx-auto">
+          <p>Servicios profesionales de reclutamiento, que se adecuan a las necesidades de tu empresa.</p>
+        </div> 
+    </div>
+
+    <!-- pausas laborales -->
+    <div class="container py-5">
+      <div class="col-lg-12">
+          <div class="row">
+            <!-- Titulo -->
+            <div class="col-lg-12 mx-auto mb-2 ">
+              <h3 class="pb-2 h3linea">Pausas laborales</h3>
+            </div>
+            <div class="col-lg-8 mx-auto mb-2 ">
+              <p>Breves intervenciones teórico-prácticas durante la jornada laboral, para plantear temáticas fundamentales para la organización a los trabajadores. En algunos casos, se entregan herramientas y tips para que puedan reforzar lo experimentado.</p>
+              <ul>
+                <li>Autocuidado; alimentación saludable.</li>
+                <li>Autocuidado; higiene del sueño</li>
+                <li>Autocuidado; ejercicios físicos, elongaciones</li>
+                <li>Autocuidado;, cómo manejar el estrés laboral. Técnicas de relajación.</li>
+                <li>Técnicas de descanso; herramientas para descansar y desconectarse; relajación con imaginería.</li>
+              </ul>
+            </div>
+            <div class="col-lg-4 mx-auto mb-2 ">
+              <img src="images/quienessomos.jpeg" alt="Imagen quiénes somos" class="img-fluid">
+            </div>
+
+            <!-- Selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Stand UP</h5>
+              <p>Tú nos entregas los candidatos que consideres deben ser parte de tu empresa, nosotros los evaluamos, entrevistamos y entregamos una terna para que escojas a quien más te haga sentido. (También te haremos sugerencias).</p>
+            </div>
+            <!-- Reclutamiento y selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Obras de teatro personalizadas</h5>
+              <p>Para todo tipo de organizaciones.</p>
+            </div>
+            <!-- Reclutamiento y selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Actividades Outdoor</h5>
+              <p>Las actividades outdoor se realizan para potenciar fortalezas y mejorar debilidades de una organización, departamento o área, donde se requiere generar un cambio importante.
+              Se pueden elegir temas o competencias a tratar y se diseñan a la medida.</p>
+            </div>
+            <!-- Reclutamiento y selección -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Coaching</h5>
+              <p>Entrenamiento para directivos que deban enfrentar el proceso de toma de decisiones, en talleres lúdicos. Son 3 sesiones de 60' elaborando un programa modular según las competencias que se quieran desarrollar o fortalecer.</p>
+            </div>
+            <!-- Intervenciones -->
+            <div class="col-lg-4 mb-4">
+              <h5 class="pt-4 pb-2 texto_azul">Intervenciones</h5>
+              <p>Para el cuidado del medio ambiente, cultural, seguridad, etc)</p>
+            </div>
+          </div>
+      </div> 
+    </div>
+
+    <div class="container-block container-blue py-5 text-center">
+        <div class="col-lg-10 mx-auto mb-2 ">
+            <h2 class="pb-2">Si necesitas más información, contáctanos</h2>
+        </div>
+        <div class="col-lg-6 mx-auto">
+          <p>Si tienes alguna duda, o deseas cotizar algún servicio puedes contactarnos. En <b>Actitud Ilimitada</b> todo es posible.</p>
+              <a href=""><button class="btn btn-primary btn-md">¡Contáctanos!</button></a>
+        </div>
+    </div>
+<?php get_footer()?>
